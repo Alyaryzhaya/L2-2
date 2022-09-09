eb3cbfa2e4bf6e038e1db27bb19e5f41d56e0a89
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-09 21:35:39 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология");
+        System.out.println("5+2");
+        System.out.println(5+2);
+
+        int speed = 17;
+        int x = speed + 3;
+
+        double d = 6.5;
+        String name = "Petya";
+
+        System.out.println(name + " ехал со скоростью "+ speed);
+
+    }
+}
+
+
